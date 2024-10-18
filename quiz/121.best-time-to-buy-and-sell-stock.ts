8aa3d74d75d39686cb99150e36db8dc0ade7ebeb
kozy10 <kentaro1002vb@gmail.com>
2024-10-17 19:01:56 -0700

    121

---
+++ quiz/121.best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=121 lang=typescript
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+function maxProfit(prices: number[]): number {
+  let left = 0;
+  let right = 1;
+  let maxProfit = 0;
+
+  while (right < prices.length) {
+    if (prices[left] < prices[right]) {
+      const profit = prices[right] - prices[left];
+      maxProfit = Math.max(maxProfit, profit);
+    } else {
+      left = right;
+    }
+    right += 1;
+  }
+  return maxProfit;
+}
+// @lc code=end
